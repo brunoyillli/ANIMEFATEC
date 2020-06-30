c5290327edd495c22f067c0083215a6d52ba6e6d
brunoyillli <bruboneet@gmail.com>
2020-06-30 20:54:28 -0300

    Ajuste de sujeira

--- src/main/java/com/fatec/anime/api/repository/AnimeRepository.java
+++ src/main/java/com/fatec/anime/api/repository/AnimeRepository.java
@@ -6,11 +6,8 @@ import java.util.Set;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
-=======
 import org.springframework.data.repository.query.Param;
->>>>>>> ba067d5d59d36a2f49c057cb470022fe77ed8875
 
 @Repository
 public interface AnimeRepository extends CrudRepository<Anime, Long>{
